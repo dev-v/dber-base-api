0514a9f0f619f1ec546d8a0bcaeca8a12fed9b12
dev-v <250912570@qq.com>
2018-01-30 16:27:40 +0800

    基础包代码提交

--- src/main/java/com/dber/base/enums/ShopStatus.java
+++ src/main/java/com/dber/base/enums/ShopStatus.java
@@ -4,7 +4,7 @@ package com.dber.base.enums;
  * <li>修改记录: ...</li>
  * <li>店铺状态</li>
  * <pre>
- *     1-新注册 2-询问门店价格中 3-重新申请门店期望价格 4-通过认证 5-正常营业 6-休业 7-关闭
+ *     1-新注册  2-审核中 3-询价中  4-营业中
  * </pre>
  * <li>其他说明: ...</li>
  *
@@ -13,13 +13,17 @@ package com.dber.base.enums;
  * @since 2018/1/11
  */
 public enum ShopStatus {
-    NEW(1,"新注册"),
-    ENQUIRY(2,"询价中"),
-    APPLY_PRICE(3,"申价中"),
-    CERTIFICATED(4,"通过认证"),
-    OPERATION(5,"营业中"),
-    SUSPEND(6,"休业中"),
-    CLOSED(7,"关闭")
+    NEW(11,"新注册"),
+    AUDITING(21,"审核中"),
+    AUDIT_FAILURE(22,"拒绝申请"),
+    ENQUIRING(31,"询价中（待提交期望门店价）"),
+    EVALUATE_BASE_PRICE(32,"评估基础价格中"),
+    ENQUIRING_HIGHER(33,"期望价过高"),
+    ENQUIRING_LOWER(34,"期望价过低"),
+    BUSINESSING_NO_SERVICE(41,"营业中（无服务）"),
+    BUSINESSING_WITH_SERVICE(42,"营业中（有服务）"),
+    BUSINESS_SUSPEND(51,"休业中"),
+    CLOSED(52,"已关店")
     ;
 
     private int value;
--- src/main/java/com/dber/base/result/Page.java
+++ src/main/java/com/dber/base/result/Page.java
@@ -1,7 +1,9 @@
 package com.dber.base.result;
 
-import java.io.Serializable;
 import java.util.Collection;
+import java.util.Collections;
+
+import com.alibaba.fastjson.annotation.JSONField;
 
 /**
  * <li>文件名称: Page.java</li>
@@ -13,11 +15,9 @@ import java.util.Collection;
  * @since 2017年12月20日
  * @author dev-v
  */
-public class Page<E> implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4250863837743850829L;
+public class Page<E> {
+
+	public static final long MAX_PAGE_SIZE = 200;
 
 	/**
 	 * 当前页，从1开始
@@ -25,7 +25,7 @@ public class Page<E> implements Serializable {
 	private long currentPage;
 
 	/**
-	 * 每页数量 (默认20)
+	 * 每页数量
 	 */
 	private long pageSize;
 
@@ -44,6 +44,50 @@ public class Page<E> implements Serializable {
 	 */
 	private Collection<E> datas;
 
+	/**
+	 * 查询条件
+	 */
+	@JSONField(serialize = false)
+	private Object condition;
+
+	/**
+	 * 排序字段
+	 */
+	@JSONField(serialize = false)
+	private String sort;
+
+	/**
+	 * @param currentPage
+	 *            当前页 从1开始
+	 * @param pageSize
+	 *            当前页行数
+	 */
+	public Page(long currentPage, long pageSize) {
+		this.currentPage = currentPage > 0 ? currentPage : 1;
+		this.pageSize = pageSize > 0 ? pageSize > MAX_PAGE_SIZE ? MAX_PAGE_SIZE : pageSize : 1;
+	}
+
+	/**
+	 * pageSize 默认20
+	 * 
+	 * @param currentPage
+	 * @see Page#Page(long, long)
+	 */
+	public Page(long currentPage) {
+		this(currentPage, 10);
+	}
+
+	/**
+	 * <pre>
+	 * currentPage 默认1
+	 * </pre>
+	 * 
+	 * @see Page#Page(long)
+	 */
+	public Page() {
+		this(1);
+	}
+
 	public long getCurrentPage() {
 		return currentPage;
 	}
@@ -56,11 +100,50 @@ public class Page<E> implements Serializable {
 		return count;
 	}
 
+	public void setCount(long count) {
+		this.count = count > -1 ? count : 0;
+		long temp = count % pageSize;
+		allPage = (count / pageSize) + (temp == 0 ? 0 : 1);
+		if (allPage < 1) {
+			currentPage = 1;
+		} else if (currentPage > allPage)
+			currentPage = allPage;
+	}
+
 	public long getAllPage() {
 		return allPage;
 	}
 
 	public Collection<E> getDatas() {
 		return datas;
+	}
+
+	public void setDatas(Collection<E> datas) {
+		this.datas = datas;
+	}
+
+	public Object getCondition() {
+		return condition;
+	}
+
+	public void setCondition(Object condition) {
+		this.condition = condition;
+	}
+
+	public long getTotal(){
+		return count;
+	}
+
+	public String getSort() {
+		return sort;
+	}
+
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void emptyDatas() {
+		this.datas = Collections.EMPTY_LIST;
 	}
 }
